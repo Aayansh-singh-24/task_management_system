36618723f9dbca57ec345bea5b80c0537d00d45f
Aayansh-singh-24 <luciferversion2.2@gmail.com>
2026-06-04 00:06:39 +0530

    creaing table

--- src/utils/db.py
+++ src/utils/db.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from src.utils.setting import setting
 
-Base = declarative_base()
+Base = declarative_base() # use to mapped python class with database and help in table creation
 
 engine = create_engine(url = setting.DB_CONNECTION) # engine = main connection system with database
 
