38dced2ae21c863b6ca7301df51730369ef04de7
Aayansh-singh-24 <luciferversion2.2@gmail.com>
2026-06-09 20:46:58 +0530

    Include task router in FastAPI application

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from fastapi import FastAPI
 from src.utils.db import Base,engine
 from src.tasks.model import TaskModel
+from src.tasks.router import task
 
 Base.metadata.create_all(engine)
 
 
 
-
 app = FastAPI(title="This is first page")
+app.include_router(task)
 
 @app.get("/")
 def home():
