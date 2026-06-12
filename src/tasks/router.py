3f8d80d5a6f2e290338732c49d6eda56594db5b5
Aayansh-singh-24 <luciferversion2.2@gmail.com>
2026-06-12 19:37:04 +0530

    Add GET endpoints for retrieving tasks

--- src/tasks/router.py
+++ src/tasks/router.py
@@ -10,3 +10,11 @@ task = APIRouter(prefix="/tasks")
 @task.post("/create")
 def task_create(body : TaskSchema,db = Depends(get_db)): # dependency injection
     return controller.create_task(body,db)
+
+@task.get("/get_all")
+def get_task(db = Depends(get_db)):
+    return controller.get_all_task(db)
+
+@task.get("/get_one_task/{id}")
+def get_one_data(id : int , db = Depends(get_db)):
+    return controller.get_one_task(id,db)
