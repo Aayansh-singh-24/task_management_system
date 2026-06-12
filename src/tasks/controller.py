fd219bf81f5bd10e6061668b4703b91a3f919a49
Aayansh-singh-24 <luciferversion2.2@gmail.com>
2026-06-12 19:36:17 +0530

    Implement task retrieval functions in controller
    Added functions to retrieve tasks from the database.

--- src/tasks/controller.py
+++ src/tasks/controller.py
@@ -1,6 +1,7 @@
 from src.tasks.dtos import TaskSchema
 from sqlalchemy.orm import Session
 from src.tasks.model import TaskModel
+from fastapi import HTTPException
 
 def create_task(body:TaskSchema,db : Session):
     data = body.model_dump()
@@ -10,3 +11,15 @@ def create_task(body:TaskSchema,db : Session):
     db.commit()
     db.refresh(new_task)
     return {"status" : "task create Sucessfully"}
+
+def get_all_task(db : Session):
+    task = db.query(TaskModel).all() # run a query on database of table whose table schema is TaskModel and extract all data
+    return {"data" : task}
+
+def get_one_task(id : int , db:Session):
+    one_task = db.query(TaskModel).get(id) # run a query on database and fetch a data of give id
+
+    if not one_task:
+        raise HTTPException(status_code=404,detail="Task not found.")
+    
+    return {"status":"Task fetch successfully","Data" : one_task}
